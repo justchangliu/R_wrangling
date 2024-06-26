3411e4c41c8ec188e8bfb71bd1f62ca43814af94
Chang Liu <cl4493@cumc.columbia.edu>
2024-06-26 17:14:02 -0400

    Update basic_R_walkthrough.R

--- basic_R_walkthrough.R
+++ basic_R_walkthrough.R
@@ -1,5 +1,6 @@
 ###### Here are the notes/reminders for myself #####
-##### Select Columns
+
+#####                 ------------Select Columns-----------             #####
 
 # use1: data %>% select(colname1, colname2, colname3,...) %>% glimpse() 
 # Note: use pipe; select based on column names; order matters
@@ -20,6 +21,31 @@
 # Note: select based on the type of the data; need to input a function that outputs boolean value
 # is.character();is.numeric();is.integer();is.double();is.logical();is.factor();is.Date()
 
-# 
+# use7: data %>% select_if(~!is.numeric(.)) %>% glimpse
+# Note: ~ is the shorthand to create an anonymous function; . is to represent each column
+# ~ is to wrap a statement as a function to be passed into a function
+# ! is to negate; na.rm is to remove NA values
+# ~mean(.,na.rm=TRUE)>10 
+# ~n_distinct(.) <20 ; n_distinct is used to count the amount of distinct values in a column
+
+# use8: data %>% select(colname3,colname2,everything()) %>% glimpse
+# can arrange order and append everything else in the end
+
+
+# use9: data %>% select_all(toupper/tolower)
+# can use select_all to select all columns
+
+#####                 ------------Column Names -----------             #####
+# use1: data %>% select(colname1, colname2, colname3 = new_colname3) %>% glimpse() 
+# rename the column within select function
+
+# use2: data %>% rename(colname1 = new1, colname2=new2, colname3 = new_colname3) %>% glimpse() 
+# rename the column within rename function without selection 
+
+# use3: data %>% select_all(~str_replace(., " ", "_"))
+# replace the "dot" in all column names from white space to _
+
+# ues4: data %>% tibble::rownames_to_column("colname") %>% head 
+# to turn a rowname into a specific column
 
 
