e32af1d2acb78be2e1c927e552e4d0ff9db38daa
Chang Liu <cl4493@cumc.columbia.edu>
2024-06-26 16:04:08 -0400

    Update basic_R_walkthrough.R

--- basic_R_walkthrough.R
+++ basic_R_walkthrough.R
@@ -1,1 +1,25 @@
+###### Here are the notes/reminders for myself #####
+##### Select Columns
+
+# use1: data %>% select(colname1, colname2, colname3,...) %>% glimpse() 
+# Note: use pipe; select based on column names; order matters
+
+# use2: data %>% select(colname1:colname4,colname7:colname10) 
+# Note: select a range of columns
+
+# use3: data %>% select(-(colname1:colname4),-colname7, column3) 
+# Note: data deselection and re-add the deselected column in the end
+
+# use4: data %>% select( colname1,starts_with("a") ) 
+# Note: use start_with();end_with();contains(); matches() to select based on matching column names
+
+# use5: data %>% select(!!vector_column_names)
+# Note: vector_column_names is a vector c("colname1","colname3","colnam5")
+
+# use6: data %>% select_if(is.numeric) %>% glimpse
+# Note: select based on the type of the data; need to input a function that outputs boolean value
+# is.character();is.numeric();is.integer();is.double();is.logical();is.factor();is.Date()
+
+# 
+
 
