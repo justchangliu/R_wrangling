6e34cf46e18632ee2970eee6ee324cbf280770be
Chang Liu <cl4493@cumc.columbia.edu>
2024-06-26 16:13:03 -0400

    Update R_packages_collection.R

--- R_packages_collection.R
+++ R_packages_collection.R
@@ -1,1 +1,15 @@
+#1 
+# pacman::p_load()
+# installs the package if necessary and loads it for use. 
+
+#2
+pacman::p_load(
+  rio,        # importing data  
+  here,       # relative file pathways  
+  janitor,    # data cleaning and tables
+  lubridate,  # working with dates
+  matchmaker, # dictionary-based cleaning
+  epikit,     # age_categories() function
+  tidyverse   # data management and visualization
+)
 
